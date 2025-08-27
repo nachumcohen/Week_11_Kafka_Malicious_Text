134f01ceb62ce573c92097394a95e3498bce8b65
benjamin <2000beni770@gmail.com>
2025-08-27 12:44:57 +0300

    check the collection size and add 100 doc limit

--- retriever/mongo_dal.py
+++ retriever/mongo_dal.py
@@ -7,6 +7,7 @@ from mongo_connection import  MongoConnection
 class Dal:
     def __init__(self):
         self.tweets_collection = self._init_collection()
+        self.retrieves_count = 0
 
     def _init_collection(self):
         connection = MongoConnection.get_connectin()
@@ -17,5 +18,20 @@ class Dal:
         return collection
 
     def get_data(self):
-        data = self.tweets_collection.find({},{"_id":0})
+        data = self.tweets_collection.find({},{"_id":0}).sort("CreateDate",1).skip(100*self.retrieves_count).limit(100)
+        self.retrieves_count += 1
         return list(data)
+
+
+    def _get_collection_size(self):
+        meta_data = self.tweets_collection.database.command("collstats", self.tweets_collection.name)
+        return meta_data["count"]
+
+
+    def check_work_finsh(self):
+        collection_size = self._get_collection_size()
+        if self.retrieves_count*100 < collection_size:
+            return True
+        print("all collection have been read.... the work finished")
+        return False
+
