3e896506ef2fad989a05bfe5b616a69c908eaf48
benjamin <2000beni770@gmail.com>
2025-08-28 16:44:45 +0300

    ramove sudo

--- scripts/dockers.bat
+++ scripts/dockers.bat
@@ -3,4 +3,4 @@ docker build -f preprocessor/Dockerfile -t iran_preprocess:1.0 .
 docker build -f enricher/Dockerfile -t iran_enricher:1.0 .
 docker build -f persister/Dockerfile -t iran_persister:1.0 .
 docker build -f dataRetrieval/Dockerfile -t data_retrieval:1.0 .
-sudo docker compose up -d
+docker compose up -d
