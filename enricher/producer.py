c7cbf1e6032e43f05305a0eabfabf96f5757157a
benjamin <2000beni770@gmail.com>
2025-08-28 10:20:43 +0300

    add producer

---
+++ enricher/producer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+import json
+
+class Producer:
+    def __init__(self):
+        self.producer = KafkaProducer(
+             bootstrap_servers=['localhost:9092'],
+             value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def produce(self, tweet):
+        if tweet["Antisemitic"]:
+            topic_name = 'enriched_preprocessed_tweets_antisemitic'
+        else:
+            topic_name = 'enriched_preprocessed_tweets_not_antisemitic'
+        self.producer.send(topic_name,tweet)
+        self.producer.flush()
