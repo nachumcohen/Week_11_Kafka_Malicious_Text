70d72b378d1ec2859f9ee76c0f5abe04671eb61a
benjamin <2000beni770@gmail.com>
2025-08-28 16:26:11 +0300

    docker building bat file

--- scripts/dockers.bat
+++ scripts/dockers.bat
@@ -1,1 +1,5 @@
 docker build -f retriever/Dockerfile -t iran_retriver:1.0 .
+docker build -f Preprocessor/Dockerfile -t iran_preprocess:1.0 .
+docker build -f enricher/Dockerfile -t iran_enricher:1.0 .
+docker build -f persister/Dockerfile -t iran_persister:1.0 .
+docker build -f dataRetrieval/Dockerfile -t data_retrieval:1.0 .
